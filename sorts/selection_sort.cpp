e4aaa0eafe2386553f26b8ec79189d5a0b4e5fe6
zotonguyen <nmnhat2528@clc.fitus.edu.vn>
2026-03-06 17:16:14 +0700

    Add files via upload
    thuật toán selection sort

---
+++ sorts/selection_sort.cpp
@@ -0,0 +1,19 @@
+#include "selection_sort.h"
+
+
+void selection_sort(int a[], int n,long long &comparison){
+    comparison = 0;
+    for(int i = 0 ;++comparison&&i<n-1;i++){
+        int minIndex = i;
+        for(int j = i+1;++comparison&&j<n;j++){
+            if(++comparison&&a[minIndex] > a[j]) minIndex = j;
+        }
+        if(minIndex != i)
+        swap(a[minIndex],a[i]);
+    }
+}
+void selection_sort(int a[], int n){
+    
+    long long comparsion;
+    selection_sort(a,n,comparsion);
+}
