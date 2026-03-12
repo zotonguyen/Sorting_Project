f4499a65d4d3b2dadf8b03306aac606085c31b14
sand012 <2512732086@student.hcmus.edu.vn>
2026-03-12 22:00:31 +0700

    Update insertionSort.cpp

--- sorts/insertionSort.cpp
+++ sorts/insertionSort.cpp
@@ -18,3 +18,19 @@ void insertionSort(int a[], int n)
 		a[j + 1] = key;
 	}
 }
+void insertionSort(int a[] , int n , long long &comparison)
+{
+	comparison = 0;
+	for (int i = 0; ++comparison && i < n; i++)
+	{
+		int key = a[i];
+		int j = i - 1;
+		while (++comparison && j >= 0 && ++comparison && a[j] > key)
+		{
+
+			a[j + 1] = a[j];
+			j = j - 1;
+		}
+		a[j + 1] = key;
+	}
+}
